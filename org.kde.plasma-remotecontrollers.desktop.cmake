6743cc9b17d1852d9f41c291964f671bf26ce719
l10n daemon script <scripty@kde.org>
2022-01-12 01:49:17 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- org.kde.plasma-remotecontrollers.desktop.cmake
+++ org.kde.plasma-remotecontrollers.desktop.cmake
@@ -2,6 +2,10 @@
 # SPDX-FileCopyrightText: 2022 Bart Ribbers <bribbers@disroot.org>
 [Desktop Entry]
 Name=Plasma Remotecontrollers
+Name[ca]=Controls remots del Plasma
+Name[es]=Controladores remotos de Plasma
+Name[nl]=Plasma afstandsbedieningen
+Name[x-test]=xxPlasma Remotecontrollersxx
 Description=Translates events from various input devices to native key presses
 Description[ca]=Tradueix esdeveniments des de diversos dispositius d'entrada a pulsacions natives de tecles
 Description[es]=Traduce eventos de diversos dispositivos de entrada a pulsaciones de teclas nativas
@@ -10,6 +14,7 @@ Description[nl]=Vertaalt gebeurtenissen uit verschillende invoerapparaten naar i
 Description[pl]=Tłumaczy zdarzenia z różnych urządzeń wejściowych na naciśnięcia klawiszy
 Description[ru]=Преобразование событий, получаемых с различных устройств, в нажатия клавиш
 Description[sk]=Prekladá udalosti z rôznych vstupných zariadení na natívne stlačenia kláves
+Description[sl]=Prevaja dogodke iz različnih vhodnih naprav v pritiske domorodnih tipk
 Description[sv]=Översätter händelser från olika typer av inmatningsenheter till inbyggda tangentnedtryckningar
 Description[uk]=Транслює події з різних пристроїв введення у сигнали про натискання клавіш
 Description[x-test]=xxTranslates events from various input devices to native key pressesxx
